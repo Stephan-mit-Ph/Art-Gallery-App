f233370d61963cc0eb253187809d7125a02e7a99
Stephan Niendorf <stephan@stephanmitph.com>
2023-10-25 02:59:00 +0200

    add navigation

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,30 +1,35 @@
-import {
-  StyledLayout,
-  StyledHeader,
-  StyledMain,
-  StyledNavigation,
-} from "./Layout.styled";
-import { Favorite } from "../Icons";
-import { Piece } from "../Icons";
-import { Spotlight } from "../Icons";
-import Link from "next/link";
+import { StyledLayout, StyledHeader, StyledMain, StyledNavigation } from './Layout.styled';
+import { Favorite } from '../Icons';
+import { Piece } from '../Icons';
+import { Spotlight } from '../Icons';
+import Link from 'next/link';
 
 const Layout = ({ children }) => {
-  return (
-    <StyledLayout>
-      <StyledHeader>
-        <Link href="/" aria-label="Homepage">
-          Art Gallery
-        </Link>
-      </StyledHeader>
-      <StyledMain>{children}</StyledMain>
-      <StyledNavigation>
-        <Piece />
-        <Spotlight />
-        <Favorite />
-      </StyledNavigation>
-    </StyledLayout>
-  );
+	return (
+		<StyledLayout>
+			<StyledHeader>
+				<Link href='/' aria-label='Homepage'>
+					Art Gallery
+				</Link>
+			</StyledHeader>
+			<StyledMain>{children}</StyledMain>
+			<StyledNavigation>
+				<Link href='/art-pieces' aria-label='Art pieces page'>
+					<Piece />
+				</Link>
+				<Link href='/' aria-label='Spotlight Page'>
+					<Spotlight />
+				</Link>
+				<Link
+					href='/'
+					aria-label='
+      Favorites page'
+				>
+					<Favorite />
+				</Link>
+			</StyledNavigation>
+		</StyledLayout>
+	);
 };
 
 export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,18 @@
-import useSWR from 'swr';
+import Head from 'next/head';
 
 import Spotlight from '@/components/Spotlight';
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function HomePage() {
-	const {
-		data: pieces,
-		isLoading,
-		error,
-	} = useSWR('https://example-apis.vercel.app/api/art', fetcher);
-
-	if (error) return <div>failed to load</div>;
-	if (isLoading) return <div>loading...</div>;
-
+export default function SpotlightPage({ pieces }) {
 	const spotlightPiece = pieces[Math.floor(Math.random() * (pieces.length - 1))];
 
 	return (
 		<>
-			<Spotlight image={spotlightPiece.imageSource} artist={spotlightPiece.artist} />
+			<Head>
+				<title>Art Gallery - Spotlight</title>
+			</Head>
+			{spotlightPiece && (
+				<Spotlight image={spotlightPiece.imageSource} artist={spotlightPiece.artist} />
+			)}
 		</>
 	);
 }
